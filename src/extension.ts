3d08c031620baa234c422100fb6ae6a00af82f4a
txl524840833 <524840833@qq.com>
2019-01-24 00:08:01 +0800

    各种配置 test

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "uihelper" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("uihelper.test", async () => {
+      //弹框
+       vscode.window.showInformationMessage("Hello World!");
+      //   vscode.window.showErrorMessage("error world");
+      //   vscode.window.showWarningMessage("warn message");
+      //   var input=await vscode.window.showInputBox();
+      //   if(input){
+      // 	vscode.window.showInformationMessage(input);
+      //   }
+      //读配置
+      //   var cf=vscode.workspace.getConfiguration("uihelper");
+      //   var tangxuelei=cf.get("tangxuelei");
+	  //   console.log(tangxuelei);
+	  
+    })
+  );
 }
-
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function alert(str: string) {
+  vscode.window.showInformationMessage(str);
+}
