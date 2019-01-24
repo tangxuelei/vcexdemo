a1627fa35c7fcaafda400d871b927791ee328e36
txl524840833 <524840833@qq.com>
2019-01-25 02:58:36 +0800

    更完美的出来了

--- src/uihelper.ts
+++ src/uihelper.ts
@@ -1,68 +1,58 @@
 import * as vscode from "vscode";
 import { window } from "vscode";
 import * as fs from "fs";
-import * as path from "path";
-var parse5=require("parse5");
+var parse5 = require("parse5");
 
 export default class UIHelper {
-    log: vscode.OutputChannel;
-    constructor() {
-        this.log= vscode.window.createOutputChannel("uihelperlog");
+  log: vscode.OutputChannel;
+  constructor() {
+    this.log = vscode.window.createOutputChannel("uihelperlog");
+  }
+  private _warn(obj: string) {
+    vscode.window.showInformationMessage(obj);
+  }
+  private _alert(obj: string) {
+    vscode.window.showErrorMessage(
+      `${obj}\nwant some help?link me!\nemail:tangxuele888@gmail.com`,
+      { modal: true }
+    );
+  }
+  html2scss() {
+    var editor = window.activeTextEditor;
+    if (!editor) {
+      return this._alert("errcode:001");
     }
-    private _warn(obj:string) {
-        vscode.window.showInformationMessage(obj);
+    if (editor.selection.isEmpty) {
+      return this._alert("you may not select any xml like code");
     }
-    private _alert(obj:string){
-        vscode.window.showErrorMessage(`${obj}\nwant some help?link me!\nemail:tangxuele888@gmail.com`,{modal:true});
+    if (editor.document.languageId !== "html") {
+      return this._alert("you may not select any xml like code");
     }
-    run(){
-        var editor=window.activeTextEditor;
-        if(!editor){
-            return this._alert("errcode:001");
-        }
-        if(editor.selection.isEmpty){
-            return this._alert("you may not select any xml like code");
-        }
-        if(editor.document.languageId!=="html"){
-            return this._alert("you may not select any xml like code");
-        }
-        var codetext=editor.document.getText(editor.selection).trim();
-        var syntaxtree=parse5.parseFragment(codetext);
-        var nodes=syntaxtree.childNodes;
-        console.log(nodes);
-        nodes.forEach(i=>{
-            delete i.parentNode;
-            delete i.namespaceURI;
-        });
-        var csstext=this._makecss(nodes);
-        fs.writeFileSync(editor.document.uri.fsPath+".scss",csstext);
-        this._warn(`file:${editor.document.uri.fsPath+".scss"} is created`);
-    }
-    private _makecss(sourceNds:Array<any>) {
-        var nodes=fltEptLine(sourceNds);
-        return `${s(nodes[0])}{
-             ${fltEptLine(nodes[0].childNodes).map(i=>s(i)+'{}').join('\n')}
-        }`;
-    }
+    var codetext = editor.document.getText(editor.selection).trim();
+    var syntaxtree = parse5.parseFragment(codetext);
+    var csstext = makecss(syntaxtree.childNodes);
+    fs.writeFileSync(editor.document.uri.fsPath + ".scss", csstext);
+    this._warn(`file:${editor.document.uri.fsPath + ".scss"} is created`);
+  }
 }
-
-function fltEptLine(node:Array<any>){
-    return node.filter(i=>{
-         return i.nodeName!=="#text";
-     });
+function fltEptLine(nodes: Array<any>) {
+  return nodes.filter(i => {
+    return i.nodeName !== "#text";
+  });
 }
-
-
-function s(node){
-    var attr=node.attrs.find(i=>i.name=='class');
-    if(attr){
-        return `${node.tagName}.${attr.value}`;
-    }else{
-        return `${node.tagName}`;
-    }
+function makeselector(node) {
+  var attr = node.attrs.find(i => i.name === "class");
+  if (attr) {
+    return `${node.tagName}.${attr.value}`;
+  } else {
+    return `${node.tagName}`;
+  }
 }
-
-function ispnode(node){
-
+function makecss(nodes) {
+  nodes = fltEptLine(nodes);
+  return nodes
+    .map(i => {
+      return `${makeselector(i)}{${makecss(i.childNodes)}}`;
+    })
+    .join("\n");
 }
-
